02205e1c7026846c43184630031e43e4e0118c4c
Saeed Mosaffer <132621749+saeedmosaffer@users.noreply.github.com>
2024-07-09 00:22:09 +0300

    Update FileHandler.java

--- Saeed Mosaffer - 1202254/src/application/FileHandler.java
+++ Saeed Mosaffer - 1202254/src/application/FileHandler.java
@@ -7,20 +7,28 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class FileHandler {
-    public static List<PCB> readProcesses(String fileName) {
-        List<PCB> processes = new ArrayList<>();
-        try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
-            String line;
-            while ((line = br.readLine()) != null) {
-                String[] parts = line.split("\\s+");
-                int id = Integer.parseInt(parts[0]);
-                int size = Integer.parseInt(parts[1]);
-                int time = Integer.parseInt(parts[2]);
-                processes.add(new PCB(id, size, time, 0, -1, -1));
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return processes;
-    }
+	public static List<PCB> readProcesses(String fileName) {
+		List<PCB> processes = new ArrayList<>();
+		try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
+			String line;
+			while ((line = br.readLine()) != null) {
+				String[] parts = line.split("\\s+");
+				if (parts.length != 3) {
+					System.err.println("Invalid line format: " + line);
+					continue;
+				}
+				try {
+					int id = Integer.parseInt(parts[0]);
+					int size = Integer.parseInt(parts[1]);
+					int time = Integer.parseInt(parts[2]);
+					processes.add(new PCB(id, size, time, 0, -1, -1));
+				} catch (NumberFormatException e) {
+					System.err.println("Invalid number format in line: " + line);
+				}
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return processes;
+	}
 }
